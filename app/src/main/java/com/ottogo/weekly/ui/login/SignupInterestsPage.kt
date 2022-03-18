9c029e3da0374b290f0c7f155ddce27a4b2ac228
nipplelion <piolotorrico@yahoo.com>
2022-03-18 15:27:25 -0700

    Add old code to SignupInterestsPage.kt

--- app/src/main/java/com/ottogo/weekly/ui/login/SignupInterestsPage.kt
+++ app/src/main/java/com/ottogo/weekly/ui/login/SignupInterestsPage.kt
@@ -4,18 +4,27 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.ottogo.weekly.api.WeeklyApi
+import com.ottogo.weekly.ui.components.CustomButton
+import com.ottogo.weekly.ui.theme.nunitoFamily
+import kotlinx.coroutines.runBlocking
 
 /*
 *
@@ -33,10 +42,46 @@ import androidx.navigation.NavController
 @Composable
 fun SignupInterestsPage(navController: NavController, token: String) {
 
+    Column(modifier = Modifier
+        .background(color = Color.White)
+        .padding(24.dp)) {
+        Text(
+            text = "Interests",
+            fontFamily = nunitoFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 42.sp
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .height(54.dp)
+                .width(328.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(color = Color(0xFFF0EAFF))
+        ) {
+            Text(
+                text = "What do you like to do? (Minimum 3)",
+                color = Color(0xFF4C21C2),
+                fontFamily = nunitoFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+            )
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "Activity Title",
+            fontFamily = nunitoFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
 
+        runBlocking {
+            WeeklyApi.retrofitService.activity(mapOf("Authorization" to "token 265245769906872d88b40205147f5cbf63538b83"))
+            Log.d("status", "Retrieved Activity List")
+        }
 
-    Column(){
-
-
+        Spacer(modifier = Modifier.height(32.dp))
+        CustomButton(buttonText = "Finish", onClick = {})
     }
 }
