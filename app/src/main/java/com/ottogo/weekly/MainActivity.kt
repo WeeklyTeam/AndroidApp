36dccb47fcbb9f94a12f70e5ad3f6c4e3c1d52c5
John Smith <thaolinda2727@gmail.com>
2022-04-05 13:56:10 -0700

    Updated LoginPage and SignUpBirthdayaPage

--- app/src/main/java/com/ottogo/weekly/MainActivity.kt
+++ app/src/main/java/com/ottogo/weekly/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.ottogo.weekly
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ottogo.weekly.ui.login.*
 import com.ottogo.weekly.ui.theme.WeeklyTheme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
 
@@ -69,10 +78,45 @@ fun LoginNavigation(){
     }
 }
 
-class MyNonActivityClass     //save the context received via constructor in a local variable
-    (  // variable to hold context
-    private val context: Context
-)
+
+@Composable
+fun showDatePicker(context: Context){
+
+    val year: Int
+    val month: Int
+    val day: Int
+
+    val calendar = Calendar.getInstance()
+    year = calendar.get(Calendar.YEAR)
+    month = calendar.get(Calendar.MONTH)
+    day = calendar.get(Calendar.DAY_OF_MONTH)
+    calendar.time = Date()
+
+    val date = remember { mutableStateOf("") }
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            date.value = "$dayOfMonth/$month/$year"
+        }, year, month, day
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Selected Date: ${date.value}")
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = {
+            datePickerDialog.show()
+        }) {
+            Text(text = "Open Date Picker")
+        }
+    }
+
+}
+
 
 
 
--- app/src/main/java/com/ottogo/weekly/ui/login/SignupBirthdayPage.kt
+++ app/src/main/java/com/ottogo/weekly/ui/login/SignupBirthdayPage.kt
@@ -2,9 +2,13 @@ package com.ottogo.weekly.ui.login
 
 import android.app.DatePickerDialog
 import android.content.Context
+import android.os.Bundle
 import android.view.ContextThemeWrapper
+import android.view.View
 import android.widget.CalendarView
 import android.widget.DatePicker
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,12 +20,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
-import com.ottogo.weekly.MyNonActivityClass
+import java.security.AccessController.getContext
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -40,9 +45,48 @@ import java.util.*
 
 @Composable
 fun SignupBirthdayPage(navController: NavController) {
-        Column(){
+    val mainContext = LocalContext.current
 
+    showDatePicker(context = mainContext)
+
+}
+
+@Composable
+fun showDatePicker(context: Context){
+
+    val year: Int
+    val month: Int
+    val day: Int
+
+    val calendar = Calendar.getInstance()
+    year = calendar.get(Calendar.YEAR)
+    month = calendar.get(Calendar.MONTH)
+    day = calendar.get(Calendar.DAY_OF_MONTH)
+    calendar.time = Date()
+
+    val date = remember { mutableStateOf("") }
+    val datePickerDialog = DatePickerDialog(
+        context,
+        {_: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            date.value = "$year/$month/$dayOfMonth"
+        }, year, month, day
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Selected Birthday: ${date.value}")
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = {
+            datePickerDialog.show()
+        }) {
+            Text(text = "Open Date Picker")
+        }
     }
+
 }
 
 
