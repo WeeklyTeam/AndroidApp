daa3977f23be81f0afd285b6009513f1f32743be
jonathanmw200 <70730100+jonathanmw200@users.noreply.github.com>
2022-04-09 14:41:08 -0800

    Added new text types, and ProfilePicture.kt

---
+++ app/src/main/java/com/ottogo/weekly/ui/components/ProfilePicture.kt
@@ -0,0 +1,14 @@
+package com.ottogo.weekly.ui.components
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ProfilePicture(url: String?) {
+
+    Column() {
+
+    }
+}
