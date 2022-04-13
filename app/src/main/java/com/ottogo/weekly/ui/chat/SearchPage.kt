17d0c0396089841ebf58d483abcaf6a2c77b38fa
nipplelion <piolotorrico@yahoo.com>
2022-04-13 10:04:36 -0700

    Add ApiList.kt

--- app/src/main/java/com/ottogo/weekly/ui/chat/SearchPage.kt
+++ app/src/main/java/com/ottogo/weekly/ui/chat/SearchPage.kt
@@ -1,10 +1,13 @@
 package com.ottogo.weekly.ui.chat
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.ottogo.weekly.viewmodels.UserViewModel
 
 @Composable
 fun SearchPage(userViewModel: UserViewModel) {
-    Column(){}
+    Column(){
+        Text(text = "Hello")
+    }
 }
