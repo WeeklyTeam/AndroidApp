d2673e1f3e91c6cd8cdd5f57da3946eaaf79edfe
nipplelion <piolotorrico@yahoo.com>
2022-04-13 10:30:33 -0700

    Edit Color.kt

--- app/src/main/java/com/ottogo/weekly/ui/chat/SearchPage.kt
+++ app/src/main/java/com/ottogo/weekly/ui/chat/SearchPage.kt
@@ -1,13 +1,21 @@
 package com.ottogo.weekly.ui.chat
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.ottogo.weekly.viewmodels.UserViewModel
 
 @Composable
 fun SearchPage(userViewModel: UserViewModel) {
-    Column(){
-        Text(text = "Hello")
+    Column(modifier = Modifier.padding(16.dp)){
+        var searchText by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            modifier = Modifier.padding())
     }
 }
